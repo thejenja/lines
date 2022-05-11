0a96e81b56317944c2b27f6253f8c3d9f83067d3
zmqp <domarlaid2@mail.ru>
2022-05-11 17:28:18 +0500

    trying to make ball touchable

--- app/src/main/java/com/example/lines/Game.java
+++ app/src/main/java/com/example/lines/Game.java
@@ -8,9 +8,12 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
 public class Game extends AppCompatActivity {
 
@@ -40,4 +43,42 @@ public class Game extends AppCompatActivity {
             tableLayout.addView(tableRow, i);
         }
     }
+    private int xDelta, yDelta;
+    private View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override public boolean onTouch(View view, MotionEvent event) {
+            final int x = (int) event.getRawX();
+            final int y = (int) event.getRawY();
+
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN: {
+                    FrameLayout.LayoutParams lParams = (FrameLayout.LayoutParams) view.getLayoutParams();
+
+                    xDelta = x - lParams.leftMargin;
+                    yDelta = y - lParams.topMargin;
+                    break;
+                }
+                case MotionEvent.ACTION_UP: {
+                    Toast.makeText(getApplicationContext(), "Объект перемещён", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                case MotionEvent.ACTION_MOVE: {
+                    if (x - xDelta + view.getWidth() <= container.getWidth()
+                            && y - yDelta + view.getHeight() <= container.getHeight()
+                            && x - xDelta >= 0
+                            && y - yDelta >= 0) {
+                        FrameLayout.LayoutParams layoutParams =
+                                (FrameLayout.LayoutParams) view.getLayoutParams();
+                        layoutParams.leftMargin = x - xDelta;
+                        layoutParams.topMargin = y - yDelta;
+                        layoutParams.rightMargin = 0;
+                        layoutParams.bottomMargin = 0;
+                        view.setLayoutParams(layoutParams);
+                    }
+                    break;
+                }
+            }
+            container.invalidate();
+            return true;
+        }
+    };
 }
